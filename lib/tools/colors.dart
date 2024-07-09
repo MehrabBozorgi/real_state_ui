b5e42684c9ca421c070fb2f727c4bc8f0c43f8d6
mehrabbozorgi <mehrabbozorgi.business@gmail.com>
2024-07-09 18:27:16 +0330

    real_state_ui

---
+++ lib/tools/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor = Color(0xFFFFFFFF);
+const searchBarColor = Color(0xFFE0E0E0);
+const primaryColor = Color(0xFF212121);
+const primaryColor2 = Color(0xff85caff);
+//
+const greyColor = Color(0xFF757575);
+const redColor = Color(0xFFE64A19);
