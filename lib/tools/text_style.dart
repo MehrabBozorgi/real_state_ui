d088ede5a29c1a2c455d2804eb6b23ae4210fed5
mehrabbozorgi <mehrabbozorgi.business@gmail.com>
2024-07-17 12:28:03 +0330

    real_state_ui

---
+++ lib/tools/text_style.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+final textColor1=TextStyle(color: Colors.grey.shade900,fontSize: 20,fontWeight: FontWeight.bold);
+final textColor2=TextStyle(color: Colors.grey.shade900, fontSize: 20,fontWeight: FontWeight.w800,);
+final textColor3=TextStyle(color: Colors.grey.shade900, fontSize: 16,fontWeight: FontWeight.bold,);
+final textColor4=TextStyle(color: Colors.grey.shade900, fontSize: 16,fontWeight: FontWeight.w800,);
+//
+const textColor5=TextStyle(color: backGroundColor, fontSize: 20,fontWeight: FontWeight.bold,);
+const textColor6=TextStyle(color: backGroundColor, fontSize: 18,fontWeight: FontWeight.bold,);
+const textColor7=TextStyle(color: backGroundColor, fontSize: 14,fontWeight: FontWeight.w700,);
+const textColor8=TextStyle(color: backGroundColor, fontSize: 14,fontWeight: FontWeight.normal,);
+//
+const textColor9=TextStyle(color: greyColor, fontSize: 16,fontWeight: FontWeight.bold,);
+const textColor10=TextStyle(color: greyColor, fontSize: 14,fontWeight: FontWeight.w800,);
+const textColor11=TextStyle(color: greyColor, fontSize: 14,fontWeight: FontWeight.w600,height: 1.3);
